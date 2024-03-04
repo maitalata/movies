95dc9b828437f1a73912dd16e1890920e6931d06
Umar Sunusi Maitalata <maitalata@gmail.com>
2024-03-04 10:57:20 +0100

    Initial commit

---
+++ src/main/java/tech/labsity/movies/MoviesApplication.java
@@ -0,0 +1,22 @@
+package tech.labsity.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//@RestController
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+//	@GetMapping("/")
+//	public String apiRoot() {
+//		return "Hello, World!";
+//	}
+
+}
